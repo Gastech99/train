6456efdbae90e630752f7f763cb0e3f144e90286
Gastech99 <diabygassamad16@gmail.com>
2023-04-03 07:05:45 +0000

    add new code

--- train.py
+++ train.py
@@ -1,1 +1,12 @@
 print("Ceci est un cours sur Git")
+
+import zlib
+s = b'witch which has which witches wrist watch'
+len(s)
+
+t = zlib.compress(s)
+len(t)
+
+zlib.decompress(t)
+b'witch which has which witches wrist watch'
+zlib.crc32(s)
